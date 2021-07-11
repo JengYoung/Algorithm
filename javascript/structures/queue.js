e7bf5c975633decd9ccb35e634347d7eeb8a2090
Jengyoung <young_pallete@naver.com>
2021-07-11 13:22:46 +0900

    structure_Queue implemented by Class

--- javascript/structures/queue.js
+++ javascript/structures/queue.js
@@ -1,12 +1,41 @@
-const arr = new Array(10).fill();
-arr.forEach((val, idx) => {
-    arr[idx] = idx + 1;
-})
-console.log(arr)
+// 그냥 단순히 만든 큐
+// const arr = new Array(10).fill();
+// arr.forEach((val, idx) => {
+//     arr[idx] = idx + 1;
+// })
+// console.log(arr)
 
-for (let i = 11; i < 21; i++) {
-    const poppedValue = arr.shift();
-    console.log(poppedValue)
-    arr.push(i);
-    console.log(arr);
+// for (let i = 11; i < 21; i++) {
+//     const poppedValue = arr.shift();
+//     console.log(poppedValue)
+//     arr.push(i);
+//     console.log(arr);
+// }
+
+class Queue {
+    constructor() {
+        this.arr = [];
+    }
+    append(value) {
+        this.arr.push(value);
+        // 원한다면 추가시마다 console로 확인 가능하도록
+        return this.arr;
+    }
+    popleft() {
+        this.arr.shift();
+        // 원한다면 추가시마다 console로 확인 가능하도록
+        return this.arr;
+    }
+
+    // 현재 큐를 볼 수 있는 메서드
+    print() {
+        return this.arr;
+    }
 }
+
+const queue = new Queue();
+queue.append(1);
+queue.append(2);
+queue.append(101);
+queue.popleft();
+console.log(queue.print());
