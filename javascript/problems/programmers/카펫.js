3031e2a01d99f69e3a810eb5077bd3e771c3b1ee
Jengyoung <young_pallete@naver.com>
2021-08-01 23:18:23 +0900

    [solve] 프로그래머스 - 카펫.js

---
+++ javascript/problems/programmers/카펫.js
@@ -0,0 +1,20 @@
+/*
+    1. brown을 통해 가로 + 세로를 구한다.
+        * (brown + 4) / 2 = (가로+세로)
+    2. for문을 통해 임의의 가로, 세로를 정한다.
+    3. 가로 및 세로를 대입하여 노랑이 적합한지를 구한다.
+        yellow: (가로 - 2) * (세로 - 2)
+    4. 리턴
+*/
+
+const getHalfPerimeter = (brown) => (brown + 4) / 2;
+const checkValid = (yellow, width, height) => ((width - 2) * (height - 2)) === yellow ? true : false
+const solution = (brown, yellow) => {
+    const halfPerimeter = getHalfPerimeter(brown);
+    for (let height = 3; height < halfPerimeter; height += 1) {
+        const width = halfPerimeter - height;
+        if (checkValid(yellow, width, height)) return [width, height]
+    }
+    // 만약 답이 없을 경우
+    return -1;
+}
