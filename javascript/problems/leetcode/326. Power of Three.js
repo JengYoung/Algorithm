d4d4d94e4d65174ed6d9cbdf274bfa8358d37b24
JengYoung <young_pallete@naver.com>
2022-03-04 15:13:33 +0900

    solve: leetcode -  326. Power Of Three.js

---
+++ javascript/problems/leetcode/326. Power of Three.js
@@ -0,0 +1,27 @@
+// 재귀로 풀었을 때
+var isPowerOfThree = function(n) {
+    if (n === 0) return false;
+    if (n === 1) return true;
+    
+    if (n % 3 !== 0) return false;
+    
+    const result = isPowerOfThree(n / 3);
+    
+    return result;
+};
+
+
+// 재귀로 풀지 않았을 때
+var isPowerOfThree = function(n) {
+    const limit = Math.pow(2, 31) - 1;
+    let num = 1;
+    
+    const powerOfThrees = [1];
+    
+    while (num <= limit) {
+        num *= 3
+        powerOfThrees.push(num);
+    }
+    
+    return powerOfThrees.includes(n);
+};
