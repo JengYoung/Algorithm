a5e7f57875705e949d8517b67f2ec9df6c11b81f
JengYoung <young_pallete@naver.com>
2022-05-07 16:17:10 +0900

    :recycle: 코드 리팩토링
    1. 큐 클래스 구현
    2. visited를 고정된 길이의 배열로 생성
    3. 삼항 연산자 활용

--- javascript/problems/programmers/피로도.js
+++ javascript/problems/programmers/피로도.js
@@ -1,14 +1,42 @@
-// 3:17
+class Queue {
+  constructor() {
+    this.arr = [];
+    this.front = 0;
+    this.rear = 0;
+    this.length = 0;
+  }
+
+  push(val) {
+    this.arr.push(val);
+    this.rear += 1;
+    this.length += 1;
+  }
+
+  shift() {
+    if (this.length) {
+      const value = this.arr[this.front];
+      delete this.arr[this.front];
+      this.front += 1;
+      this.length -= 1;
+      return value;
+    }
+  }
+}
+
 const solution = (k, dungeons) => {
   let maxCount = 0;
   const dungeonsCount = dungeons.length;
-  const queue = [];
+  const queue = new Queue();
+  const makeVisited = (count) => new Array(count).fill(false);
 
   for (let i = 0; i < dungeonsCount; i += 1) {
     const [minFatigue, fatigueCost] = dungeons[i];
 
     if (k >= minFatigue) {
-      queue.push([k - fatigueCost, 1, [i]]);
+      const visited = makeVisited(dungeonsCount);
+      visited[i] = true;
+
+      queue.push([k - fatigueCost, 1, visited]);
     }
   }
 
@@ -16,26 +44,26 @@ const solution = (k, dungeons) => {
     if (maxCount === dungeonsCount) return maxCount;
 
     const [nowFatigue, nowCount, visited] = queue.shift();
-    if (visited.length === dungeonsCount) {
-      if (maxCount < nowCount) {
-        maxCount = nowCount;
-      }
 
+    if (visited.every((v) => v)) {
+      if (maxCount < nowCount) maxCount = nowCount;
       continue;
     }
 
     for (let i = 0; i < dungeonsCount; i += 1) {
-      if (visited.includes(i)) continue;
+      if (visited[i]) continue;
 
       const [minFatigue, fatigueCost] = dungeons[i];
+      const nextVisited = visited.map((value, idx) => idx === i || value);
 
-      if (nowFatigue >= minFatigue) {
-        queue.push([nowFatigue - fatigueCost, nowCount + 1, [...visited, i]]);
-      } else {
-        queue.push([nowFatigue, nowCount, [...visited, i]]);
-      }
+      queue.push(
+        nowFatigue >= minFatigue
+          ? [nowFatigue - fatigueCost, nowCount + 1, nextVisited]
+          : [nowFatigue, nowCount, nextVisited]
+      );
     }
   }
+
   return maxCount;
 };
 
