4f02c8f15ddda5a2b4f86b6019c1aa12edbd4a38
JengYoung <young_pallete@naver.com>
2022-08-07 12:22:28 +0900

    :white_check_mark: [PROBLEM-96] leetcode - diagonal traverse

--- javascript/problems/leetcode/498. Diagonal Traverse.js
+++ javascript/problems/leetcode/498. Diagonal Traverse.js
@@ -4,7 +4,7 @@
  */
 const findDiagonalOrder = (mat) => {
   const result = [];
-  console.log(mat);
+
   const rowLength = mat.length;
   const colLength = mat[0].length;
 
@@ -13,12 +13,8 @@ const findDiagonalOrder = (mat) => {
   let i = 0;
   let j = 0;
 
-  console.log(diagonalCnt, rowLength, colLength);
   for (let nowCnt = 0; nowCnt < diagonalCnt; nowCnt += 1) {
-    console.log(`start - nowCnt: ${nowCnt} / `, i, j);
-
     while (i >= 0 && j >= 0 && i < rowLength && j < colLength) {
-      console.log(`nowCnt: ${nowCnt} / `, i, j);
       result.push(mat[i][j]);
       if (nowCnt % 2 === 0) {
         i -= 1;
@@ -29,41 +25,22 @@ const findDiagonalOrder = (mat) => {
       }
     }
 
-    console.log(`mid: / `, i, j);
     if (nowCnt % 2 === 0) {
       if (j > colLength - 1) {
         i += 2;
         j = colLength - 1;
       } else {
-        i = i < 0 ? 0 : rowLength - 1;
+        i = 0;
       }
     } else {
       if (i > rowLength - 1) {
         i -= 1;
         j += 2;
       } else {
-        j = j < 0 ? 0 : colLength - 1;
+        j = 0;
       }
     }
-    console.log('result: ', i, j);
   }
 
   return result;
 };
-
-console.log(
-  findDiagonalOrder([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ])
-);
-
-console.log(
-  findDiagonalOrder([
-    [1, 2],
-    [3, 4],
-  ])
-);
-
-console.log(findDiagonalOrder([[3], [2]]));
