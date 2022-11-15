def7b469dfaa8ed7625804e3af91a67578710b19
JengYoung <young_pallete@naver.com>
2022-11-16 00:36:33 +0900

    :fire: 순열 주석 제거

--- javascript/problems/leetcode/permutations.js
+++ javascript/problems/leetcode/permutations.js
@@ -1,20 +1,21 @@
-const permute = nums => {
+const permute = (nums) => {
   const result = [];
   if (nums.length === 1) return [nums];
 
   nums.forEach((head, idx) => {
-    console.log("head: ", head)
-    permute(nums.filter((_, tailIdx) => tailIdx !== idx))
-    .forEach(permutedTail => {
-      console.log("여기: ", [head, ...permutedTail]);
-      result.push([head, ...permutedTail])
-    })
-  })
+    console.log('head: ', head);
+    permute(nums.filter((_, tailIdx) => tailIdx !== idx)).forEach(
+      (permutedTail) => {
+        console.log('여기: ', [head, ...permutedTail]);
+        result.push([head, ...permutedTail]);
+      }
+    );
+  });
 
   return result;
-}
+};
 
 (() => {
-  const nums = [1,2,3,4]
-  console.log(permute(nums))
-})()
+  const nums = [1, 2, 3, 4];
+  console.log(permute(nums));
+})();
