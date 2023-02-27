ee714833f68de8889c8eb825dfd17c0b2a1a4c58
JengYoung <young_pallete@naver.com>
2023-02-27 12:06:53 +0900

    :truck: arrange leetcode problems
    점점 보기가 헷갈려져서, 문제의 번호 순서에 따라 디렉토리를 추가로 분류했다.

--- javascript/problems/leetcode/0~100/11. container-with-most-water.js
+++ javascript/problems/leetcode/0~100/11. container-with-most-water.js
@@ -1,5 +1,5 @@
 // 핵심은 가장 두 개 선과 인덱스 차이.
-const maxArea = height => {
+const maxArea = (height) => {
   let start = 0;
   let end = height.length - 1;
   let maxArea = 0;
@@ -16,16 +16,16 @@ const maxArea = height => {
   function getArea(startHeight, endHeight) {
     return Math.min(startHeight, endHeight) * (end - start);
   }
-  
-  return maxArea
-}
+
+  return maxArea;
+};
 
 (() => {
-  const height = [1,8,6,2,5,4,8,3,7];
+  const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
   console.log(maxArea(height));
 })();
 
 (() => {
-  const height = [1,2,3,4,5,25,24,3,4]
+  const height = [1, 2, 3, 4, 5, 25, 24, 3, 4];
   console.log(maxArea(height));
-})()
+})();
