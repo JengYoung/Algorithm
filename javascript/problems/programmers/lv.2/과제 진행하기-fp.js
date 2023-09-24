38b41b88b61818fe33139ccca520d5d6960e45c2
JengYoung <young_pallete@naver.com>
2023-09-24 15:37:11 +0900

    :white_check_mark: [PROBLEM-224] 프로그래머스 - 과제 진행하기(FP)

---
+++ javascript/problems/programmers/lv.2/과제 진행하기-fp.js
@@ -0,0 +1,133 @@
+const getTime = (time) => {
+  return time
+    .split(':')
+    .reduce((acc, cur, index) => acc + (!index ? 60 * +cur : +cur), 0);
+};
+
+const getSortedPlans = (plans) => {
+  return plans
+    .map(([name, time, period]) => [name, getTime(time), +period])
+    .sort((a, b) => a[1] - b[1]);
+};
+
+const isNoLength = (arr) => !arr.length;
+
+const getArrangedProgress = (
+  result,
+  stack,
+  breakTime,
+  getNextResult,
+  getNextStack
+) => {
+  if (isNoLength(stack)) {
+    return { result, stack };
+  }
+
+  const lastItem = stack.at(-1);
+
+  const [nowName, nowTime, nowPeriod] = lastItem;
+
+  const remainTime = nowPeriod - breakTime;
+
+  const nextResult = getNextResult(result, remainTime <= 0, nowName);
+  const nextStack = getNextStack(stack, remainTime > 0, [
+    nowName,
+    nowTime,
+    remainTime,
+  ]);
+
+  if (remainTime > 0) {
+    return { breakTime: breakTime - nowPeriod, result, stack: nextStack };
+  }
+
+  return getArrangedProgress(
+    nextResult,
+    nextStack,
+    breakTime - nowPeriod,
+    getNextResult,
+    getNextStack
+  );
+};
+
+const getNextResult = (result, flag, name) => {
+  return flag ? [...result, name] : result;
+};
+
+const getNextStack = (stack, flag, item) => {
+  return stack.slice(0, -1).concat(flag ? [item] : []);
+};
+
+const progress = (
+  sortedPlans,
+  index,
+  result,
+  stack,
+  getNextResult,
+  getNextStack
+) => {
+  if (index === sortedPlans.length) {
+    return result.concat([...stack].reverse().map((v) => v[0]));
+  }
+
+  const breakTime =
+    index > 0 ? sortedPlans[index][1] - sortedPlans[index - 1][1] : 0;
+
+  const { result: nextResult, stack: nextStack } = getArrangedProgress(
+    result,
+    stack,
+    breakTime,
+    getNextResult,
+    getNextStack
+  );
+
+  return progress(
+    sortedPlans,
+    index + 1,
+    nextResult,
+    [...nextStack, sortedPlans[index]],
+    getNextResult,
+    getNextStack
+  );
+};
+
+function solution(plans) {
+  return progress(
+    getSortedPlans(plans),
+    0,
+    [],
+    [],
+    getNextResult,
+    getNextStack
+  );
+}
+
+(() => {
+  console.log(
+    solution([
+      ['korean', '11:40', '30'],
+      ['english', '12:10', '20'],
+      ['math', '12:30', '40'],
+    ])
+  );
+})();
+
+(() => {
+  console.log(
+    solution([
+      ['science', '12:40', '50'],
+      ['music', '12:20', '40'],
+      ['history', '14:00', '30'],
+      ['computer', '12:30', '100'],
+    ])
+  );
+})();
+
+(() => {
+  console.log(
+    solution([
+      ['aaa', '12:00', '20'],
+      ['bbb', '12:10', '30'],
+      ['ccc', '12:40', '10'],
+    ])
+  );
+})();
