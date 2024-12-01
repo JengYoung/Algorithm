3e999ed4300c697f1475a4a03646cbbf8f0f7c50
JengYoung <young_pallete@naver.com>
2024-12-01 23:29:26 +0900

    :recycle: 메뉴 리뉴얼, 완주하지 못한 선수, 베스트앨범 다시 풀고 리팩토링

--- javascript/problems/programmers/완주하지 못한 선수.js
+++ javascript/problems/programmers/완주하지 못한 선수.js
@@ -1,73 +1,39 @@
-const copyArray = arr => [...arr];
-const sortArray = arr => {
-    return go(
-        arr,
-        copyArray
-    ).sort()
-}
+const solution = (participant, completion) => {
+  const map = new Map();
 
-function checkIfSameValue(left, right) {
-    return (left === right) ? true : false;
-}
+  let result = 0;
 
-function makeSameLangthArrays(firstArr, secondArr) {
-    const lengthDifference = firstArr.length - secondArr.length;
-    if (!lengthDifference) return [firstArr, secondArr]
-    const concatArray = new Array(Math.abs(lengthDifference)).fill(null);
-    return (lengthDifference > 0) 
-        ? [firstArr, secondArr.concat(concatArray)] 
-        : [firstArr.concat(concatArray), secondArr];
-}
-/* 
-    배열끼리의 각 인덱스 값을 비교하면서 필터링해줍니다. 
-    만약 다른 길이라면 통일시켜서 다른 길이만큼 다른 값으로 처리합니다.
-*/
-function filterDifferentValuesByIndex(comparedArray) {
-    return function* (iter) {
-        const [firstArr,secondArr] = makeSameLangthArrays(iter, comparedArray);
-        const iterIterator = firstArr[Symbol.iterator]();
-        const comparedArrayIterator = secondArr[Symbol.iterator]();
-        while ((!(iterNext = iterIterator.next()).done && !(cmpNext = comparedArrayIterator.next()).done)) {
-            if (!checkIfSameValue(cmpNext.value, iterNext.value)) yield iterNext.value;
-        }
-    } 
-}
+  participant.forEach((p, index) => {
+    map.set(p, (map.get(p) ?? []).concat([index]));
 
-function reduce(f) {
-    return function (acc, iter) {
-        if (!iter) acc = (iter = acc[Symbol.iterator]()).next().value;
-        for (const a of iter) acc = f(acc, a);
-        return acc;
-    }
-}
+    result ^= index;
+  });
 
-function go(arg, ...funcs) {
-    return reduce((a, f) => f(a))(arg, funcs);
-}
+  const targetIndex = completion.reduce((acc, cur) => {
+    return (acc ^= map.get(cur).pop());
+  }, result);
 
-function take(limit) {
-    return function *(iter) {
-        for (const a of iter) {
-            yield a;
-            if (--limit === 0) break;
-        }
-    }
-}
-function destructureArray([iter]) {
-    return iter
-}
-const findFailedBySortedArrays = (participant, completion) => {
-    return go(
-        participant,
-        filterDifferentValuesByIndex(completion), // comparedArray: completion, iter: participant
-        take(1),
-        destructureArray
+  return participant[targetIndex];
+};
+
+console.log(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki']));
+console.log(
+  solution(
+    ['marina', 'josipa', 'nikola', 'vinko', 'filipa'],
+    ['josipa', 'filipa', 'marina', 'nikola']
+  )
+);
+
+console.log(
+  solution(['mislav', 'stanko', 'mislav', 'ana'], ['stanko', 'ana', 'mislav'])
+);
+
+var solution = (participant: string[], completion: string[]) => {
+  return participant.find(
+    (participant) => !completion[participant]--,
+    completion.map(
+      (participant) =>
+        (completion[participant] = (completion[participant] | 0) + 1)
     )
-}
-const solution = (participants, completion) => {
-    return findFailedBySortedArrays(sortArray(participants), sortArray(completion))
-}
-
-const participants = ["marina", "josipa", "nikola", "filipa", "vinko"];
-const completion = ["josipa", "filipa", "marina", "nikola"];
-console.log(solution(participants, completion))
+  );
+};
