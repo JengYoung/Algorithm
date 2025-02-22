f6fb20fbb620339256c26fd554691043abcd10fe
황재영 <jengyoung.developer@gmail.com>
2025-02-23 00:11:23 +0900

    ♻️ Leetcode - 128. set 활용하여 리팩토링
    Runtime performance 97ms -> 33ms 개선(Beats 88.95%)

--- javascript/problems/leetcode/100~200/128. Longest Consecutive Sequence.js
+++ javascript/problems/leetcode/100~200/128. Longest Consecutive Sequence.js
@@ -3,54 +3,24 @@
  * @return {number}
  */
 const longestConsecutive = function (nums) {
-  const positives = {};
-  const negatives = {};
-
-  for (let i = 0; i < nums.length; i += 1) {
-    const num = nums[i];
-    if (num >= 0) {
-      positives[num] = null;
-    } else {
-      negatives[num * -1] = null;
-    }
-  }
-
-  const sortedArray = [];
-  const sortedNegatives = Object.keys(negatives);
-  const sortedPositives = Object.keys(positives);
-
-  while (sortedNegatives.length) {
-    sortedArray.push(sortedNegatives.pop() * -1);
-  }
-
-  for (let i = 0; i < sortedPositives.length; i += 1) {
-    sortedArray.push(sortedPositives[i]);
-  }
-
   let result = 0;
 
-  let count = 0;
-  let prev = 0;
+  const set = new Set(nums);
 
-  for (let i = 0; i < sortedArray.length; i += 1) {
-    const now = sortedArray[i];
-
-    if (!i) {
-      result = 1;
-      count += 1;
-      prev = now;
-      continue;
+  set.forEach((value) => {
+    if (set.has(value - 1)) {
+      return;
     }
 
-    if (Number(now) === Number(prev) + 1) {
-      count += 1;
-    } else {
-      count = 1;
+    let currentValue = value;
+    let cnt = 1;
+    while (set.has(currentValue + 1)) {
+      cnt += 1;
+      currentValue += 1;
     }
 
-    result = Math.max(result, count);
-    prev = now;
-  }
+    result = Math.max(result, cnt);
+  });
 
   return result;
 };
